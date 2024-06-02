4896200d54df48ca59c71f5b6a4a77558a712fd3
imdj <os@imadij.com>
2024-06-02 10:40:05 +0300

    Use Iconoir icon library

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -46,6 +46,7 @@
         {#if opmlText === ""}
             <div class="flex w-full h-96 grow overflow-y-auto relative mt-4 p-2 border-2 rounded-xl items-center justify-center bg-slate-100">
                 <label for="opmlFile" class="absolute text-lg m-auto opacity-70 z-10">
+                    <div class="text-center"><svg xmlns="http://www.w3.org/2000/svg" class="inline-block w-16 text-gray-500 mb-4" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M6 20h12m-6-4V4m0 0l3.5 3.5M12 4L8.5 7.5"/></svg></div>
                     Upload an OPML file or <button class="text-sky-500 hover:text-sky-700" on:click={() => { opmlText = xmlFormat(opml.createEmpty()) }}>Create a new one</button>
                 </label>
                 <input type="file" accept=".opml" on:change={handleFileSelect} class="opacity-0 w-full h-full cursor-pointer"/>
@@ -60,12 +61,14 @@
     </div>
 </main>
 
-<footer class="flex flex-wrap justify-center items-center gap-2 mt-4 mb-2">
+<footer>
+    <div class="flex flex-wrap container mx-auto justify-center items-center gap-2 mt-4 mb-2">
     <p>
         By <a href="https://www.imadij.com">Imad Aldij</a>
     </p>
-    <a class="flex flex-row space-x-2 md:space-x-4 bg-gray-100 border border-gray-300 hover:bg-gray-200 items-center font-semibold rounded-md p-2 md:py-2 md:px-3" href="https://github.com/imdj/opml-editor">
-        <svg class="h-6 w-auto inline-block" width="98" height="96" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 98 96"><path fill-rule="evenodd" clip-rule="evenodd" d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z" fill="#24292f"/></svg>
+    <a class="flex flex-row space-x-2 bg-gray-100 border border-gray-300 hover:bg-gray-200 items-center font-semibold rounded-md p-2 md:py-2 md:px-3" href="https://github.com/imdj/opml-editor">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-auto inline-block" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><path d="M16 22.027v-2.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7a5.44 5.44 0 0 0-1.5-3.75a5.07 5.07 0 0 0-.09-3.77s-1.18-.35-3.91 1.48a13.4 13.4 0 0 0-7 0c-2.73-1.83-3.91-1.48-3.91-1.48A5.07 5.07 0 0 0 5 5.797a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7a3.37 3.37 0 0 0-.94 2.58v2.87"/><path d="M9 20.027c-3 .973-5.5 0-7-3"/></g></svg>
         <span class="flex-none">Source code on GitHub</span>
     </a>
+    </div>
 </footer>
