b03dd991ad11a57d64af8d31db0706d82af19cf4
imdj <os@imadij.com>
2024-06-02 09:37:41 +0300

    Exclude codemirror/lang-xml package from vite optimizeDeps

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	optimizeDeps: {
-		exclude: ["svelte-codemirror-editor", "codemirror", "@codemirror/language-javascript"],
+		exclude: ["svelte-codemirror-editor", "codemirror", "@codemirror/lang-xml"]
 	},
 });
