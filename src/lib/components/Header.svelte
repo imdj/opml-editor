188262f3a5d0f83bc0d2750109a8c5db9453c96f
imdj <os@imadij.com>
2024-06-02 12:32:48 +0300

    componentize header and footer

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,22 @@
+<script>
+    let isDropdownOpen = false;
+
+    const handleDropdownFocusLoss = ({ relatedTarget, currentTarget }) => {
+        // check if the new focus target doesn't present in the dropdown tree (exclude ul\li padding area because relatedTarget, in this case, will be null)
+        if (relatedTarget instanceof HTMLElement && currentTarget.contains(relatedTarget)) return;
+        isDropdownOpen = false
+    }
+</script>
+
+<header class="container mx-auto flex justify-between items-center h-12 mt-2 p-2">
+    <div class="relative inline-block ml-auto" on:focusout={handleDropdownFocusLoss}>
+        <button on:click={() => isDropdownOpen = !isDropdownOpen}>
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-8" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 5h8m-8 7h13M3 19h18"/></svg>
+        </button>
+        <ul class="absolute shadow-2xl w-48 right-0 rounded-lg bg-white p-1" style:visibility={isDropdownOpen ? 'visible' : 'hidden'}>
+            <li><a class="block p-2 rounded hover:bg-slate-200" href="/">Home</a></li>
+            <li><a class="block p-2 rounded hover:bg-slate-200" href="/opml">About OPML</a></li>
+            <li><a class="block p-2 rounded hover:bg-slate-200" href="/about">About the editor</a></li>
+        </ul>
+    </div>
+</header>
