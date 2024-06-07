43ba605d6dda4f7ac898f7c343df4aeb45a2dcdb
imdj <os@imadij.com>
2024-06-07 17:36:47 +0300

    Change text for github button

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -5,7 +5,7 @@
         </p>
         <a class="flex flex-row space-x-2 bg-gray-100 border border-gray-300 hover:bg-gray-200 items-center font-semibold rounded-md p-2 no-underline md:py-2 md:px-3" href="https://github.com/imdj/opml-editor">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-auto inline-block" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><path d="M16 22.027v-2.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7a5.44 5.44 0 0 0-1.5-3.75a5.07 5.07 0 0 0-.09-3.77s-1.18-.35-3.91 1.48a13.4 13.4 0 0 0-7 0c-2.73-1.83-3.91-1.48-3.91-1.48A5.07 5.07 0 0 0 5 5.797a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7a3.37 3.37 0 0 0-.94 2.58v2.87"/><path d="M9 20.027c-3 .973-5.5 0-7-3"/></g></svg>
-            <span class="flex-none">Source code on GitHub</span>
+            <span class="flex-none">View on GitHub</span>
         </a>
     </div>
 </footer>
