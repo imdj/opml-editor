e9382fa337ef4393b4b660fbd8b59216753ce9f5
imdj <os@imadij.com>
2024-06-09 09:49:34 +0300

    Use the subscription list example provided by the opml spec

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -32,19 +32,37 @@ So that's where this editor comes in, it's a simple tool to help you manage your
 
 <h2>Basic example</h2>
 <pre class="overflow-auto bg-gray-200 p-4 rounded-md">
-    &lt;?xml version="1.0" encoding="ISO-8859-1"?&gt;
-    &lt;opml version="2.0"&gt;
-        &lt;head&gt;
-            &lt;title&gt;Example OPML&lt;/title&gt;
-        &lt;/head&gt;
-        &lt;body&gt;
-            &lt;outline text="Example Outline" title="Example Outline"&gt;
-                &lt;outline text="Item 1" title="Item 1" xmlUrl="http://example.com/rss.xml" htmlUrl="http://example.com/"&gt;
-                &lt;outline text="Item 2" title="Item 2" xmlUrl="http://example.com/rss.xml" htmlUrl="http://example.com/"&gt;
-                &lt;outline text="Item 3" title="Item 3" xmlUrl="http://example.com/rss.xml" htmlUrl="http://example.com/"&gt;
-            &lt;/outline&gt;
-        &lt;/body&gt;
-    &lt;/opml&gt;
+&lt;?xml version=&quot;1.0&quot; encoding=&quot;ISO-8859-1&quot;?&gt;
+&lt;opml version=&quot;2.0&quot;&gt;
+    &lt;head&gt;
+        &lt;title&gt;mySubscriptions.opml&lt;/title&gt;
+        &lt;dateCreated&gt;Sat, 18 Jun 2005 12:11:52 GMT&lt;/dateCreated&gt;
+        &lt;dateModified&gt;Tue, 02 Aug 2005 21:42:48 GMT&lt;/dateModified&gt;
+        &lt;ownerName&gt;Dave Winer&lt;/ownerName&gt;
+        &lt;ownerEmail&gt;dave@scripting.com&lt;/ownerEmail&gt;
+        &lt;expansionState&gt;&lt;/expansionState&gt;
+        &lt;vertScrollState&gt;1&lt;/vertScrollState&gt;
+        &lt;windowTop&gt;61&lt;/windowTop&gt;
+        &lt;windowLeft&gt;304&lt;/windowLeft&gt;
+        &lt;windowBottom&gt;562&lt;/windowBottom&gt;
+        &lt;windowRight&gt;842&lt;/windowRight&gt;
+    &lt;/head&gt;
+    &lt;body&gt;
+        &lt;outline text=&quot;CNET News.com&quot; description=&quot;Tech news and business reports by CNET News.com. Focused on information technology, core topics include computers, hardware, software, networking, and Internet media.&quot; htmlUrl=&quot;http://news.com.com/&quot; language=&quot;unknown&quot; title=&quot;CNET News.com&quot; type=&quot;rss&quot; version=&quot;RSS2&quot; xmlUrl=&quot;http://news.com.com/2547-1_3-0-5.xml&quot;/&gt;
+        &lt;outline text=&quot;washingtonpost.com - Politics&quot; description=&quot;Politics&quot; htmlUrl=&quot;http://www.washingtonpost.com/wp-dyn/politics?nav=rss_politics&quot; language=&quot;unknown&quot; title=&quot;washingtonpost.com - Politics&quot; type=&quot;rss&quot; version=&quot;RSS2&quot; xmlUrl=&quot;http://www.washingtonpost.com/wp-srv/politics/rssheadlines.xml&quot;/&gt;
+        &lt;outline text=&quot;Scobleizer: Microsoft Geek Blogger&quot; description=&quot;Robert Scoble&#39;s look at geek and Microsoft life.&quot; htmlUrl=&quot;http://radio.weblogs.com/0001011/&quot; language=&quot;unknown&quot; title=&quot;Scobleizer: Microsoft Geek Blogger&quot; type=&quot;rss&quot; version=&quot;RSS2&quot; xmlUrl=&quot;http://radio.weblogs.com/0001011/rss.xml&quot;/&gt;
+        &lt;outline text=&quot;Yahoo! News: Technology&quot; description=&quot;Technology&quot; htmlUrl=&quot;http://news.yahoo.com/news?tmpl=index&amp;amp;cid=738&quot; language=&quot;unknown&quot; title=&quot;Yahoo! News: Technology&quot; type=&quot;rss&quot; version=&quot;RSS2&quot; xmlUrl=&quot;http://rss.news.yahoo.com/rss/tech&quot;/&gt;
+        &lt;outline text=&quot;Workbench&quot; description=&quot;Programming and publishing news and comment&quot; htmlUrl=&quot;http://www.cadenhead.org/workbench/&quot; language=&quot;unknown&quot; title=&quot;Workbench&quot; type=&quot;rss&quot; version=&quot;RSS2&quot; xmlUrl=&quot;http://www.cadenhead.org/workbench/rss.xml&quot;/&gt;
+        &lt;outline text=&quot;Christian Science Monitor | Top Stories&quot; description=&quot;Read the front page stories of csmonitor.com.&quot; htmlUrl=&quot;http://csmonitor.com&quot; language=&quot;unknown&quot; title=&quot;Christian Science Monitor | Top Stories&quot; type=&quot;rss&quot; version=&quot;RSS&quot; xmlUrl=&quot;http://www.csmonitor.com/rss/top.rss&quot;/&gt;
+        &lt;outline text=&quot;Dictionary.com Word of the Day&quot; description=&quot;A new word is presented every day with its definition and example sentences from actual published works.&quot; htmlUrl=&quot;http://dictionary.reference.com/wordoftheday/&quot; language=&quot;unknown&quot; title=&quot;Dictionary.com Word of the Day&quot; type=&quot;rss&quot; version=&quot;RSS&quot; xmlUrl=&quot;http://www.dictionary.com/wordoftheday/wotd.rss&quot;/&gt;
+        &lt;outline text=&quot;The Motley Fool&quot; description=&quot;To Educate, Amuse, and Enrich&quot; htmlUrl=&quot;http://www.fool.com&quot; language=&quot;unknown&quot; title=&quot;The Motley Fool&quot; type=&quot;rss&quot; version=&quot;RSS&quot; xmlUrl=&quot;http://www.fool.com/xml/foolnews_rss091.xml&quot;/&gt;
+        &lt;outline text=&quot;InfoWorld: Top News&quot; description=&quot;The latest on Top News from InfoWorld&quot; htmlUrl=&quot;http://www.infoworld.com/news/index.html&quot; language=&quot;unknown&quot; title=&quot;InfoWorld: Top News&quot; type=&quot;rss&quot; version=&quot;RSS2&quot; xmlUrl=&quot;http://www.infoworld.com/rss/news.xml&quot;/&gt;
+        &lt;outline text=&quot;NYT &amp;gt; Business&quot; description=&quot;Find breaking news &amp;amp; business news on Wall Street, media &amp;amp; advertising, international business, banking, interest rates, the stock market, currencies &amp;amp; funds.&quot; htmlUrl=&quot;http://www.nytimes.com/pages/business/index.html?partner=rssnyt&quot; language=&quot;unknown&quot; title=&quot;NYT &amp;gt; Business&quot; type=&quot;rss&quot; version=&quot;RSS2&quot; xmlUrl=&quot;http://www.nytimes.com/services/xml/rss/nyt/Business.xml&quot;/&gt;
+        &lt;outline text=&quot;NYT &amp;gt; Technology&quot; description=&quot;&quot; htmlUrl=&quot;http://www.nytimes.com/pages/technology/index.html?partner=rssnyt&quot; language=&quot;unknown&quot; title=&quot;NYT &amp;gt; Technology&quot; type=&quot;rss&quot; version=&quot;RSS2&quot; xmlUrl=&quot;http://www.nytimes.com/services/xml/rss/nyt/Technology.xml&quot;/&gt;
+        &lt;outline text=&quot;Scripting News&quot; description=&quot;It&#39;s even worse than it appears.&quot; htmlUrl=&quot;http://www.scripting.com/&quot; language=&quot;unknown&quot; title=&quot;Scripting News&quot; type=&quot;rss&quot; version=&quot;RSS2&quot; xmlUrl=&quot;http://www.scripting.com/rss.xml&quot;/&gt;
+        &lt;outline text=&quot;Wired News&quot; description=&quot;Technology, and the way we do business, is changing the world we know. Wired News is a technology - and business-oriented news service feeding an intelligent, discerning audience. What role does technology play in the day-to-day living of your life? Wired News tells you. How has evolving technology changed the face of the international business world? Wired News puts you in the picture.&quot; htmlUrl=&quot;http://www.wired.com/&quot; language=&quot;unknown&quot; title=&quot;Wired News&quot; type=&quot;rss&quot; version=&quot;RSS&quot; xmlUrl=&quot;http://www.wired.com/news_drop/netcenter/netcenter.rdf&quot;/&gt;
+    &lt;/body&gt;
+&lt;/opml&gt;
 </pre>
 
 <style>
