83ef8e7bb2b05df8de5316a815fa37cf88a79629
imdj <os@imadij.com>
2024-07-11 18:46:11 +0300

    Organize actions in toolbar into menus

---
+++ src/lib/components/ActionMenu.svelte
@@ -0,0 +1,50 @@
+<script>
+    let { name, icon, items } = $props();
+
+    let showMenu = $state(false);
+
+    function performAction(callback) {
+        callback();
+        showMenu = false;
+    }
+
+    function handleMenu(node) {
+        function onClickOutside(event) {
+            if (!node.contains(event.target)) {
+                showMenu = false;
+            }
+        }
+
+        document.body.addEventListener('click', onClickOutside);
+
+        return {
+            destroy() {
+                document.body.removeEventListener('click', onClickOutside);
+            }
+        };
+    }
+</script>
+
+<div class="relative" use:handleMenu>
+    <button type="button" class="flex flex-row gap-2 py-1 px-2 whitespace-nowrap items-center rounded-lg hover:bg-slate-200" onclick={() => showMenu = !showMenu}>
+        {#if icon}
+            {@html icon}
+        {/if}
+        <span>{name}</span>
+    </button>
+
+    <div class="absolute shadow-2xl w-48 mt-2 rounded-lg bg-white p-1 z-30" class:hidden={!showMenu}>
+        <ul>
+            {#each items as item}
+                <li>
+                    <button type="button" class="flex flex-row gap-2 py-1 px-2 w-full whitespace-nowrap items-center rounded hover:bg-slate-200" onclick={() => performAction(item.callback)}>
+                        {#if item.icon}
+                            {@html item.icon}
+                        {/if}
+                        <span>{item.name}</span>
+                    </button>
+                </li>
+            {/each}
+        </ul>
+    </div>
+</div>
--- src/lib/components/ToolBar.svelte
+++ src/lib/components/ToolBar.svelte
@@ -1,11 +1,39 @@
 <script>
     import {getContext} from "svelte";
     import xmlFormat from "xml-formatter";
+    import ActionMenu from "$lib/components/ActionMenu.svelte";
     import {opmlDoc} from "$lib/opml.svelte.js";
 
     let { outlinerView = $bindable() } = $props()
 
     const opml = getContext("state")
+
+    const FileMenu = [
+        {
+            name: "Save file",
+            icon: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M6 20h12M12 4v12m0 0l3.5-3.5M12 16l-3.5-3.5"/></svg>`,
+            callback: saveToFile
+        },
+        {
+            name: "Add file",
+            icon: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M22 14V8.5M6 13V6a3 3 0 0 1 3-3h5m2.992 1h3m3 0h-3m0 0V1m0 3v3M12 21H6a4 4 0 0 1 0-8h12a4 4 0 1 0 4 4v-3"/></svg>`,
+            callback: mergeFile
+        }
+    ]
+
+    const EditMenu = $state([
+        {
+            name: "Remove duplicates",
+            icon: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><path d="M7 2h9.5L21 6.5V19"/><path d="M11 22h5.5a1.5 1.5 0 0 0 1.5-1.5V8.749a.6.6 0 0 0-.176-.425l-3.148-3.148A.6.6 0 0 0 14.25 5H4.5A1.5 1.5 0 0 0 3 6.5V13m-1.008 6h6"/><path d="M14 5v3.4a.6.6 0 0 0 .6.6H18"/></g></svg>`,
+            callback: () => opml.removeOutlineDupes(),
+            disabled: opml.numOfFeeds < 2
+        },
+        {
+            name: "Reformat file",
+            icon: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="1.5"><path stroke-linecap="round" d="m3 21l10-10m5-5l-2.5 2.5"/><path d="m9.5 2l.945 2.555L13 5.5l-2.555.945L9.5 9l-.945-2.555L6 5.5l2.555-.945zm9.5 8l.54 1.46L21 12l-1.46.54L19 14l-.54-1.46L17 12l1.46-.54z"/></g></svg>`,
+            callback: () => opml.rawContent = xmlFormat(opml.rawContent),
+        }
+    ])
 
     function switchView() {
         outlinerView = !outlinerView;
@@ -69,39 +97,31 @@
         <div class="flex flex-row flex-grow gap-1 overflow-x-auto">
             <button class="flex flex-row gap-2 py-1 px-2 whitespace-nowrap items-center rounded-lg hover:bg-slate-200" onclick={selectAll} disabled={!opml.body.children.length}>
                 <input type="checkbox" checked={all_selected} class="mr-1"/>
-                {all_selected? 'Deselect all' : 'Select all'}
+                <span class="hidden sm:inline-block">{all_selected? 'Deselect all' : 'Select all'}</span>
             </button>
             <button class="flex flex-row gap-2 py-1 px-2 whitespace-nowrap items-center rounded-lg hover:bg-slate-200" onclick={() => opml.deleteSelected()} disabled={!opml.selectedItems.size}>
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-width="1.5"><path stroke-linecap="round" stroke-linejoin="round" d="M14.871 19.121L16.993 17m2.121-2.121L16.993 17m0 0l-2.122-2.121M16.993 17l2.121 2.121"/><path d="M4 9.4V4.6a.6.6 0 0 1 .6-.6h4.8a.6.6 0 0 1 .6.6v4.8a.6.6 0 0 1-.6.6H4.6a.6.6 0 0 1-.6-.6Zm0 10v-4.8a.6.6 0 0 1 .6-.6h4.8a.6.6 0 0 1 .6.6v4.8a.6.6 0 0 1-.6.6H4.6a.6.6 0 0 1-.6-.6Zm10-10V4.6a.6.6 0 0 1 .6-.6h4.8a.6.6 0 0 1 .6.6v4.8a.6.6 0 0 1-.6.6h-4.8a.6.6 0 0 1-.6-.6Z"/></g></svg>
                 Remove selected
             </button>
         </div>
-        <button class="flex flex-row gap-2 py-1 px-2 whitespace-nowrap items-center rounded-tr-lg sm:rounded-lg border-l-2 sm:border-0 sm:bg-white text-black hover:bg-gray-200 sm:hover:bg-slate-200" onclick={switchView}>
+        <button class="flex flex-row gap-2 py-1 px-2 whitespace-nowrap items-center rounded-lg bg-white text-black hover:bg-slate-200" onclick={switchView}>
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M13.5 6L10 18.5m-3.5-10L3 12l3.5 3.5m11-7L21 12l-3.5 3.5"/></svg>
-            <span class="hidden sm:inline-block">Raw OPML view</span>
+            <span>Raw OPML view</span>
         </button>
     {:else}
-        <div class="flex flex-row flex-grow gap-1 overflow-x-auto">
-            <button class="flex flex-row gap-2 py-1 px-2 whitespace-nowrap items-center rounded-lg hover:bg-slate-200" onclick={saveToFile}>
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M6 20h12M12 4v12m0 0l3.5-3.5M12 16l-3.5-3.5"/></svg>
-                Save file
-            </button>
-            <button class="flex flex-row gap-2 py-1 px-2 whitespace-nowrap items-center rounded-lg hover:bg-slate-200" onclick={mergeFile}>
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M22 14V8.5M6 13V6a3 3 0 0 1 3-3h5m2.992 1h3m3 0h-3m0 0V1m0 3v3M12 21H6a4 4 0 0 1 0-8h12a4 4 0 1 0 4 4v-3"/></svg>
-                Add file
-            </button>
-            <button class="flex flex-row gap-2 py-1 px-2 whitespace-nowrap items-center rounded-lg hover:bg-slate-200" onclick={() => opml.removeOutlineDupes()} disabled={opml.numOfFeeds < 2}>
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><path d="M7 2h9.5L21 6.5V19"/><path d="M11 22h5.5a1.5 1.5 0 0 0 1.5-1.5V8.749a.6.6 0 0 0-.176-.425l-3.148-3.148A.6.6 0 0 0 14.25 5H4.5A1.5 1.5 0 0 0 3 6.5V13m-1.008 6h6"/><path d="M14 5v3.4a.6.6 0 0 0 .6.6H18"/></g></svg>
-                Remove duplicates
-            </button>
-            <button class="flex flex-row gap-2 py-1 px-2 whitespace-nowrap items-center rounded-lg hover:bg-slate-200" onclick={() => opml.rawContent = xmlFormat(opml.rawContent)}>
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="1.5"><path stroke-linecap="round" d="m3 21l10-10m5-5l-2.5 2.5"/><path d="m9.5 2l.945 2.555L13 5.5l-2.555.945L9.5 9l-.945-2.555L6 5.5l2.555-.945zm9.5 8l.54 1.46L21 12l-1.46.54L19 14l-.54-1.46L17 12l1.46-.54z"/></g></svg>
-                Reformat file
-            </button>
+        <div class="flex flex-row flex-grow gap-1">
+            <ActionMenu
+                    name="File"
+                    icon='<svg xmlns="http://www.w3.org/2000/svg" class="h-6" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><path d="M4 21.4V2.6a.6.6 0 0 1 .6-.6h11.652a.6.6 0 0 1 .424.176l3.148 3.148A.6.6 0 0 1 20 5.75V21.4a.6.6 0 0 1-.6.6H4.6a.6.6 0 0 1-.6-.6M8 10h8m-8 8h8m-8-4h4"/><path d="M16 2v3.4a.6.6 0 0 0 .6.6H20"/></g></svg>'
+                    items={FileMenu}/>
+            <ActionMenu
+                name="Edit"
+                icon='<svg xmlns="http://www.w3.org/2000/svg" class="h-6" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M13 10V3L5 14h6v7l8-11z"/></svg>'
+                items={EditMenu}/>
         </div>
-        <button class="flex flex-row gap-2 py-1 px-2 whitespace-nowrap items-center rounded-tr-lg sm:rounded-lg border-l-2 sm:border-0 sm:bg-white text-black hover:bg-gray-200 sm:hover:bg-slate-200" onclick={switchView}>
+        <button class="flex flex-row gap-2 py-1 px-2 whitespace-nowrap items-center rounded-lg bg-white text-black hover:bg-slate-200" onclick={switchView}>
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><path d="M3 13c3.6-8 14.4-8 18 0"/><path d="M12 17a3 3 0 1 1 0-6a3 3 0 0 1 0 6"/></g></svg>
-            <span class="hidden sm:inline-block">Outliner view</span>
+            <span>Outliner view</span>
         </button>
     {/if}
 </div>
