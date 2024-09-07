5e4be84458031f3d35f0668f14e2d9abfad2e9d4
imdj <os@imadij.com>
2024-09-07 10:49:01 +0300

    Fix missing favicon in Bing

--- src/lib/components/Head.svelte
+++ src/lib/components/Head.svelte
@@ -5,6 +5,10 @@
 </script>
 
 <svelte:head>
+    <link rel="icon" href="favicon.ico" type="image/x-icon" />
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg">
+    <link rel="icon" type="image/png" href="/favicon.png">
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
