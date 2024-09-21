3c7720a3129256b0a5b1c5875a5749ae5c931690
Imad Aldij <os@imadij.com>
2024-09-21 15:33:31 +0300

    increase contrast and improve visibility for modes buttons in toolbar

--- src/lib/components/ToolBar.svelte
+++ src/lib/components/ToolBar.svelte
@@ -154,10 +154,10 @@
                 items={EditMenu}/>
     </div>
     <div class="flex flex-row gap-1 ml-2">
-        <button class="py-1 px-2 whitespace-nowrap items-center rounded-lg text-black hover:bg-slate-200" class:bg-slate-200={view === viewMode.CODE} onclick={switchView} title="Raw OPML view">
+        <button class="py-1 px-2 whitespace-nowrap items-center rounded-lg hover:bg-slate-200 hover:text-black {view === viewMode.CODE ? 'bg-black text-white' : 'text-black'}" onclick={switchView} title="Raw OPML view">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M13.5 6L10 18.5m-3.5-10L3 12l3.5 3.5m11-7L21 12l-3.5 3.5"/></svg>
         </button>
-        <button class="py-1 px-2 whitespace-nowrap items-center rounded-lg text-black hover:bg-slate-200" class:bg-slate-200={view === viewMode.OUTLINER} onclick={switchView} title="Outliner view">
+        <button class="py-1 px-2 whitespace-nowrap items-center rounded-lg hover:bg-slate-200 hover:text-black {view === viewMode.OUTLINER ? 'bg-black text-white' : 'text-black'}" onclick={switchView} title="Outliner view">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><path d="M3 13c3.6-8 14.4-8 18 0"/><path d="M12 17a3 3 0 1 1 0-6a3 3 0 0 1 0 6"/></g></svg>
         </button>
     </div>
